ccce09f77959a30984e3c4de9bd39a35d545a004
Kenhubhub <98609221+Kenhubhub@users.noreply.github.com>
2022-10-04 20:58:00 +0100

    check weather api

--- src/apiFunctions/weatherApi.js
+++ src/apiFunctions/weatherApi.js
@@ -2,6 +2,7 @@ import Axios from "axios"
 const getWeatherInfo = async (lon,lat) =>{
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=d0a10211ea3d36b0a6423a104782130e`
     const weatherInfo = Axios.get(url);
+    console.log(weatherInfo);
     return weatherInfo;
 
 }
