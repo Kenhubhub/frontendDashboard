fc4aaa31e3189f6aa08ddc829e1ea0977540d9ce
Kenhubhub <98609221+Kenhubhub@users.noreply.github.com>
2022-10-04 23:04:34 +0100

    added https instead of http to api call

--- src/apiFunctions/weatherApi.js
+++ src/apiFunctions/weatherApi.js
@@ -1,6 +1,6 @@
 import Axios from "axios"
 const getWeatherInfo = async (lon,lat) =>{
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=10a4fd005327481f998633d618c8627f`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=10a4fd005327481f998633d618c8627f`
     const weatherInfo = Axios.get(url);
     
     return weatherInfo;
